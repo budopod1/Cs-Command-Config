66ad7178c8948b797bb41b3ecf0e504280b1f905
Martin Staab <martin.e.staab@gmail.com>
2025-06-01 16:05:34 -0700

    Added: JSONConfigParser now allows additional data extraction  from the JSON, JSONConfigParser.GetShape is now public and static

--- JSONConfigParser.cs
+++ JSONConfigParser.cs
@@ -1,11 +1,12 @@
 using CsJSONTools;
 
 namespace CsCommandConfig;
-public class JSONConfigParser(ProgramConfiguration config, int priority = 0) {
+public class JSONConfigParser(ProgramConfiguration config, Action<ShapedJSON> extraParser, int priority = 0) {
     readonly ProgramConfiguration config = config;
+    readonly Action<ShapedJSON> extraParser = extraParser;
     readonly int priority = priority;
 
-    IJSONShape GetShape() {
+    public static IJSONShape GetShape(ProgramConfiguration config) {
         Dictionary<string, IJSONShape> keysShape = [];
         foreach (IOption option in config.GetOptions()) {
             if (option is IJSONCompatibleOption jsonOption) {
@@ -17,7 +18,8 @@ public class JSONConfigParser(ProgramConfiguration config, int priority = 0) {
 
     public void Parse(string file, Action<string> useFileText) {
         IJSONValue jsonValue = JSONTools.ParseJSONFile(file, useFileText);
-        ShapedJSON shaped = new(jsonValue, GetShape());
+        ShapedJSON shaped = new(jsonValue, GetShape(config));
+        extraParser?.Invoke(shaped);
         foreach (KeyValuePair<string, ShapedJSON> pair in shaped.IterObject()) {
             IJSONCompatibleOption option = (IJSONCompatibleOption)config.GetOption(pair.Key);
             try {
